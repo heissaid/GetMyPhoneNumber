e7692bc34eb0fce22c96b96f6cd9b0836626bac4
heissaid <13429326035@139.com>
2016-06-29 15:14:58 +0800

    获取手机通讯录

---
+++ src/com/example/getmyphonenumber/PhoneInfo.java
@@ -0,0 +1,29 @@
+package com.example.getmyphonenumber;
+
+public class PhoneInfo {
+
+	private String name;
+	private String number;
+	
+	public PhoneInfo(String name,String number) {
+		setNumber(number);
+		setName(name);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	
+	
+	
+}
